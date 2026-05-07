e58c2052aeb8b427bba806a9721c66197cebe11b
code-savan <eric.marvelboy@gmail.com>
2026-05-07 12:30:57 +0100

    latest update

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -5,7 +5,7 @@ const Partners = () => {
     <section className="w-full flex flex-col items-center py-12 md:py-32">
       <div className="text-center mb-6">
         <p className="text-zinc-900 text-base md:text-lg font-normal">
-          We have industry expert who have<br />partnered with
+          We have industry experts who have<br />partnered with
         </p>
       </div>
       <div className="flex justify-center">
